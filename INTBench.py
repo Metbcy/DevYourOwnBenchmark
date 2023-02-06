4c1ed8d0a9eb3089d1e5772ec7d66fdf44f1dd6c
Metbcy <Amirbredy1@gmail.com>
2023-02-05 19:04:21 -0500

    Create INTBench.py

---
+++ INTBench.py
@@ -0,0 +1,10 @@
+import time
+def benchmark():
+    start = time.perf_counter()
+    result = 0
+    for i in range(10**10):
+        result += 1
+    for i in range(5 * 10**9):
+        result *= 2
+    for i in range(2 * 10**9):
+        result /= 2
