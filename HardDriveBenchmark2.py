6775887e3c12ebbe7e9fd133ca41df2635914f95
Metbcy <Amirbredy1@gmail.com>
2023-02-05 20:48:02 -0500

    Update HardDriveBenchmark2.py

--- HardDriveBenchmark2.py
+++ HardDriveBenchmark2.py
@@ -4,6 +4,12 @@ def benchmark():
     with open("benchmark.txt", "wb") as file:
         for i in range(10**9 // 10000):
             file.write(b'0' * 10000)
-
+    result = 0
+    with open("benchmark.txt", "rb") as file:
+        chunk = file.read(10000)
+        while chunk:
+            result += len(chunk)
+            chunk = file.read(10000)
+            
     end = time.perf_counter()
     print("Time elapsed: ", end - start, "seconds")
