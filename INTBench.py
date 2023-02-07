3ed5c4ab25878207e144ab0e51c38062c72ebd1a
Metbcy <Amirbredy1@gmail.com>
2023-02-07 13:43:34 -0500

    Update INTBench.py
    Edited for readability

--- INTBench.py
+++ INTBench.py
@@ -1,13 +1,30 @@
 import time
-def benchmark():
+def addition():
+    number1 = 67
+    number2 = 30
+    for i in range(10):
+        for o in range(10):
+            result = number1 + number2
+            
+def multiplication():           
+    number1 = 89
+    number2 = 70
+    for i in range(5):
+        for o in range(10):
+            for p in range(9):
+                result = number1 * number2
+def division():
+    number1 = 100
+    number2 = 20
+    for i in range(2):
+        for o in range(10):
+            for p in range(9):
+                result = number1 / number2
+if __name__ == '__main__':
     start = time.perf_counter()
-    result = 0
-    for i in range(10**10):
-        result += 1
-    for i in range(5 * 10**9):
-        result *= 2
-    for i in range(2 * 10**9):
-        result /= 2
+    addition()
+    multiplication()
+    division()
     end = time.perf_counter()
     print("Time elapsed: ", end - start, "seconds")
-benchmark()
+ 
