8091710cd18c4f4f189d5e25c0a0ef49c1930f27
Metbcy <Amirbredy1@gmail.com>
2023-02-07 17:44:01 -0500

    Update FloatingPointBenchmark.py

--- FloatingPointBenchmark.py
+++ FloatingPointBenchmark.py
@@ -1,32 +1,31 @@
+#INTEGER BENCHMARK
 import time
-def floatingaddition():
-    number1 = 67
-    number2 = 30
+
+def addition():
+    number1 = 500.5234
+    number2 = 32.12893
     for i in range(10):
         for o in range(10):
             result = number1 + number2
             
-def floatingmultiplication():           
-    number1 = 89
-    number2 = 70
+def multiplication():           
+    number1 = 60.3549827
+    number2 = 56.4178
     for i in range(5):
-        for o in range(10):
-            for p in range(9):
-                result = number1 * number2
-def floatingdivision():
-    number1 = 100
-    number2 = 20
+        for o in range(10**9):
+            result = number1 * number2
+def division():
+    number1 = 100.4462346
+    number2 = 20.241254
     for i in range(2):
-        for o in range(10):
-            for p in range(9):
-                result = number1 / number2
+        for o in range(10**9):
+            result = number1 / number2
 
     
 if __name__ == '__main__':
     start = time.perf_counter()
-    floatingaddition()
-    floatingmultiplication()
-    floatingdivision()
+    addition()
+    multiplication()
+    division()
     end = time.perf_counter()
     print("Time elapsed: ", end - start, "seconds")
- 
