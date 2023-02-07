0c222d26653124c1f7d269b5245e5eae0312175b
Metbcy <Amirbredy1@gmail.com>
2023-02-07 13:46:33 -0500

    Update FloatingPointBenchmark.py

--- FloatingPointBenchmark.py
+++ FloatingPointBenchmark.py
@@ -1,13 +1,32 @@
 import time
-def benchmark():
+def floatingaddition():
+    number1 = 67
+    number2 = 30
+    for i in range(10):
+        for o in range(10):
+            result = number1 + number2
+            
+def floatingmultiplication():           
+    number1 = 89
+    number2 = 70
+    for i in range(5):
+        for o in range(10):
+            for p in range(9):
+                result = number1 * number2
+def floatingdivision():
+    number1 = 100
+    number2 = 20
+    for i in range(2):
+        for o in range(10):
+            for p in range(9):
+                result = number1 / number2
+
+    
+if __name__ == '__main__':
     start = time.perf_counter()
-    result = 0.0
-    for i in range(10**10):
-        result += 0.1
-    for i in range(5 * 10**9):
-        result *= 2.0
-    for i in range(2 * 10**9):
-        result /= 2.0
+    floatingaddition()
+    floatingmultiplication()
+    floatingdivision()
     end = time.perf_counter()
     print("Time elapsed: ", end - start, "seconds")
-benchmark()
+ 
