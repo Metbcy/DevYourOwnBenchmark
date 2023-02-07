ccf47ae61cf69b31fc6a8a0a3ae0a50f92170f54
Metbcy <Amirbredy1@gmail.com>
2023-02-07 17:18:36 -0500

    Update ArrayBenchmark.py

--- ArrayBenchmark.py
+++ ArrayBenchmark.py
@@ -1,13 +1,31 @@
-import array
+import os
 import time
-def benchmark():
+
+def write_to_memory(num_elements, element_size):
+    array = bytearray(num_elements * element_size)
+    for i in range(0, num_elements, element_size):
+        element = os.urandom(element_size)
+        array[i:i + element_size] = element
+    return array
+
+def read_from_memory(array, num_elements, element_size):
+    for i in range(0, num_elements, element_size):
+        element = array[i:i + element_size]
+
+if __name__ == "__main__":
+    num_elements = 5 * (10 ** 9)
+    element_size = 4
+
     start = time.perf_counter()
-    data = array.array("I", [0] * (5 * 10**9))
-    for i in range(5 * 10**9):
-        data[i] = i
-    result = 0
-    for i in data:
-        result += i
+    array = write_to_memory(num_elements, element_size)
     end = time.perf_counter()
-    print("Time elapsed: ", end - start, "seconds")
-benchmark()
+
+    write_time = end - start
+    print(f'Memory write benchmark execution time: {write_time}')
+
+    start = time.perf_counter()
+    read_from_memory(array, num_elements, element_size)
+    end = time.perf_counter()
+
+    read_time = end - start
+    print(f'Memory read benchmark execution time: {read_time}')
