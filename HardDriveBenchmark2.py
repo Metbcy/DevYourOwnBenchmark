55b72d311de8474d41f108f2be6134218ad47a3a
Metbcy <Amirbredy1@gmail.com>
2023-02-07 18:05:54 -0500

    Update HardDriveBenchmark2.py

--- HardDriveBenchmark2.py
+++ HardDriveBenchmark2.py
@@ -1,27 +1,16 @@
 import time
-import os
-
-def write():
+def benchmark():
+    start = time.perf_counter()
     with open("benchmark.txt", "wb") as file:
-        for i in range(10**9 // 100):
-            file.write(b'0' * 100)
- 
-    
-def read():
+        for i in range(10**9 // 10000):
+            file.write(b'0' * 10000)
     result = 0
     with open("benchmark.txt", "rb") as file:
-        chunk = file.read(100)
+        chunk = file.read(10000)
         while chunk:
             result += len(chunk)
-            chunk = file.read(100)
-    return result
-
-
-if __name__ == '__main__':
-    start = time.perf_counter()
-    write()
-    read()
-    result=read()
+            chunk = file.read(10000)
+            
     end = time.perf_counter()
     print("Time elapsed: ", end - start, "seconds")
-     
+benchmark()
